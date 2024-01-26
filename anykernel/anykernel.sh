886a353d1c66c1af861c63a0bc1619fdfdcedcb9
aamirr-ali <amirwide@gmail.com>
2024-01-26 23:43:10 +0530

    script: Fix for Aliz-1.0

--- anykernel/anykernel.sh
+++ anykernel/anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Realme GT Neo 2 / Realme X50 Pro B I G S H O T
+kernel.string=Realme X50 Pro / Realme GT Neo 2  A A M I R A L I
 do.devicecheck=0
 do.modules=0
 do.systemless=1
--- zip.sh
+++ zip.sh
@@ -2,6 +2,6 @@ cp out/arch/arm64/boot/Image ./anykernel/
 cp out/arch/arm64/boot/dtbo.img ./anykernel/
 
 
-cd anykernel && zip -r Bigshot-kernel-AOSP-$(date +"%d-%m-%Y"-%H%M).zip * && mv Bigshot-kernel-AOSP-$(date +"%d-%m-%Y-%H%M").zip ../out/
+cd anykernel && zip -r Aliz-kernel-1.0-$(date +"%d-%m-%Y"-%H%M).zip * && mv Aliz-kernel-v1.0-$(date +"%d-%m-%Y-%H%M").zip ../out/
 
 cd ..
